26b9fd9c07581e25be17c7cf3ba0bb2f0f264c94
souzlays <souzalaayss@gmail.com>
2024-09-14 17:15:59 -0300

    instalando tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Karla', 'sans-serif'],
+        // Você pode adicionar outras famílias de fontes aqui
+      },
+    },
     screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
+      'xs': '375px', // Para dispositivos móveis pequenos
+      'sm': '640px', // Para tablets
+      'md': '768px', // Para tablets maiores
+      'lg': '1024px', // Para laptops
+      'xl': '1440px', // Para desktops
     },
   }
 }
