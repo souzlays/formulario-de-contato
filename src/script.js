becff5a0da8fa766a573d77dac0891cc81cd6714
souzlays <souzalaayss@gmail.com>
2024-09-23 23:16:13 -0300

    validação dos campos

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
 const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const email = document.querySelector('.email');
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 function setError(index){
     campos[index].classList.add('border-red-500');
-    spans[index].classList.remove('hidden');
+    spans[index].classList.remove('hidden');  
 }
 
 function removeError(index){
     campos[index].classList.remove('border-red-500');
-    spans[index].classList.add('hidden');
+    spans[index].classList.add('hidden');   
 }
 
 function validacaoCaracteres() {
@@ -27,4 +28,18 @@ function validacaoCaracteres() {
    }); 
 };
 
-// function validacaoEmail
+function validacaoEmail(){
+    if(!emailRegex.test(campos[2].value)){
+        return setError(2);
+    }else {
+        return removeError(2);
+    }
+};
+
+function validarBotao(){
+    campos.forEach((campos, index) => {
+        if(campos.value == ""){
+            return setError(index);
+        } 
+    });
+}
