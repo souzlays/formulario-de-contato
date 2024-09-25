a7a5032d24aa3f77e623c5114472e959f2198180
souzlays <souzalaayss@gmail.com>
2024-09-25 03:07:31 -0300

    botões checkbox e radio validados

--- src/script.js
+++ src/script.js
@@ -3,14 +3,31 @@ const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
 const email = document.querySelector('.email');
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const tipoConsulta = document.querySelector('#consulta');
+
+
 
 form.addEventListener('submit', (event) => {
         event.preventDefault();
         validacaoCaracteres();
         validacaoEmail();
         validarCheckBox();
+        validarRadioButtons();
 
     });
+
+function validarRadioButtons() {
+    const spanRadioButtons = document.getElementById('#"errorMessage')
+    const radioButtons = document.querySelectorAll('input[type="radio"]');
+    const atLeastOneRadioButtonMarked = Array.from(radioButtons).some(item => item.checked)
+    if(atLeastOneRadioButtonMarked){
+        errorMessage.classList.add('hidden');
+        errorMessage.classList.remove('border-red-500');
+    }else {
+        errorMessage.classList.remove('hidden');
+        errorMessage.classList.add('border-red-500');   
+    }
+}    
 
 function setError(index){
     campos[index].classList.add('border-red-500');
@@ -48,6 +65,6 @@ function validarCheckBox(){
             spanCheckBox.style.display = 'block';
         }else{
             spanCheckBox.style.display = 'none';  
-        }
-    
+        }   
 }
+
